50069866cdd2db1e3ffc59a8165c810f30165701
odrail <vbmanu@gmail.com>
2025-07-03 22:37:13 +0200

    build iife bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,43 @@
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 
-export default {
-	input: 'src/index.ts',
-	output: [
+const baseConfig = {
+  input: 'src/index.ts',
+  output: [
     {
-		file: 'dist/bundle.js',
-		format: 'es'
-  	},
-    {
-      file: 'dist/bundle.min.js',
+      file: 'dist/bundle.js',
       format: 'es',
-      plugins: [terser()]
     }
-],
-  plugins: [typescript()]
+  ],
+  plugins: [
+    typescript(),
+  ],
 };
+
+const iifeConfig = {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/bundle.iife.js',
+      format: 'iife',
+      name: 'HattrickApiClient',
+    },
+    {
+      file: 'dist/bundle.iife.min.js',
+      format: 'iife',
+      name: 'HattrickApiClient',
+      plugins: [terser()],
+    },
+  ],
+  plugins: [
+    typescript(),
+    nodeResolve(),
+    commonjs(),
+    nodePolyfills(),
+  ],
+};
+
+export default [baseConfig, iifeConfig];
